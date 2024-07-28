6f570c5f2287e620cde973a563bd3b51488963ee
Jeffy2600 <157212974+Jeffy2600@users.noreply.github.com>
2024-07-28 13:50:40 +0700

    Update index.js

--- my-bot/index.js
+++ my-bot/index.js
@@ -1,1 +1,40 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
 
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+  }
+});
+
+client.commands = new Collection();
+
+const commands = [];
+
+// Load commands from command.js
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+  client.commands.set(command.data.name, command);
+}
+
+client.application.commands.set(commands);
+
+client.login(process.env.TOKEN);
